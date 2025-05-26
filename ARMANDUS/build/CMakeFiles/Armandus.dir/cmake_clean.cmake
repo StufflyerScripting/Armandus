e14c57930f0ad3f72fe55fa5236be45d0ed29a2c
StufflyerScripting <167232463+StufflyerScripting@users.noreply.github.com>
2025-05-26 20:53:39 +0200

    Add files via upload

---
+++ ARMANDUS/build/CMakeFiles/Armandus.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/bin/Armandus.exe"
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/bin/Armandus.exe.manifest"
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/bin/Armandus.pdb"
+  "CMakeFiles/Armandus.dir/C_/glad/src/glad.c.obj"
+  "CMakeFiles/Armandus.dir/C_/glad/src/glad.c.obj.d"
+  "CMakeFiles/Armandus.dir/src/core/app/application.cpp.obj"
+  "CMakeFiles/Armandus.dir/src/core/app/application.cpp.obj.d"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui.cpp.obj"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui.cpp.obj.d"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_draw.cpp.obj"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_draw.cpp.obj.d"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_impl_glfw.cpp.obj"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_impl_glfw.cpp.obj.d"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_impl_opengl3.cpp.obj"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_impl_opengl3.cpp.obj.d"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_tables.cpp.obj"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_tables.cpp.obj.d"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_widgets.cpp.obj"
+  "CMakeFiles/Armandus.dir/src/core/gui/imgui_widgets.cpp.obj.d"
+  "libArmandus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Armandus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
