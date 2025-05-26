3cf1992281915d6e38ee7b96428de8b723d69c64
StufflyerScripting <167232463+StufflyerScripting@users.noreply.github.com>
2025-05-26 20:56:56 +0200

    Add files via upload

---
+++ build/CMakeFiles/Armandus.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/glad/src/glad.c" "CMakeFiles/Armandus.dir/C_/glad/src/glad.c.obj" "gcc" "CMakeFiles/Armandus.dir/C_/glad/src/glad.c.obj.d"
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/core/app/application.cpp" "CMakeFiles/Armandus.dir/src/core/app/application.cpp.obj" "gcc" "CMakeFiles/Armandus.dir/src/core/app/application.cpp.obj.d"
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/core/gui/imgui.cpp" "CMakeFiles/Armandus.dir/src/core/gui/imgui.cpp.obj" "gcc" "CMakeFiles/Armandus.dir/src/core/gui/imgui.cpp.obj.d"
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/core/gui/imgui_draw.cpp" "CMakeFiles/Armandus.dir/src/core/gui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/Armandus.dir/src/core/gui/imgui_draw.cpp.obj.d"
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/core/gui/imgui_impl_glfw.cpp" "CMakeFiles/Armandus.dir/src/core/gui/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/Armandus.dir/src/core/gui/imgui_impl_glfw.cpp.obj.d"
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/core/gui/imgui_impl_opengl3.cpp" "CMakeFiles/Armandus.dir/src/core/gui/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/Armandus.dir/src/core/gui/imgui_impl_opengl3.cpp.obj.d"
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/core/gui/imgui_tables.cpp" "CMakeFiles/Armandus.dir/src/core/gui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/Armandus.dir/src/core/gui/imgui_tables.cpp.obj.d"
+  "C:/Users/carei/OneDrive/Documents/ARMANDUS/src/core/gui/imgui_widgets.cpp" "CMakeFiles/Armandus.dir/src/core/gui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/Armandus.dir/src/core/gui/imgui_widgets.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
